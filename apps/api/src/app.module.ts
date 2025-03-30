4a942805ebe8615c7c182ce184fbb220a97fd0a5
Rafael Dantas Boeira <rafaeldantasboeira@gmail.com>
2025-03-30 12:42:33 -0300

    inicializando schemas de user, project e post

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectService } from './project/project.service';
+import { ProjectModule } from './project/project.module';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { PostService } from './post/post.service';
+import { PostController } from './post/post.controller';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProjectModule, UserModule, PostModule],
+  controllers: [AppController, PostController],
+  providers: [AppService, ProjectService, UserService, PostService],
 })
 export class AppModule {}
