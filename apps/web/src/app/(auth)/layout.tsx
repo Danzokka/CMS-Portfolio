ccfe597ae5fed03598beb6c8e29581138413f16c
Danzokka <rafaeldantasboeira@gmail.com>
2025-06-01 19:39:40 -0300

    refactor(web): remove unnecessary HTML structure from layout component

--- apps/web/src/app/(auth)/layout.tsx
+++ apps/web/src/app/(auth)/layout.tsx
@@ -6,13 +6,9 @@ const layout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <html lang="en" className="h-full">
-      <body>
         <div className="w-full h-full min-h-[calc(100vh-6rem)] flex items-center justify-center bg-[url('/blobs/waves.svg')] bg-no-repeat bg-cover bg-center">
           {children}
         </div>
-      </body>
-    </html>
   );
 };
 
