5ac0814d0e148d962e18252726b7a514cf8db6a5
Rafael Dantas <rafaeldantasboeira@gmail.com>
2025-07-02 12:32:42 -0300

    feat(web): update skills data with new technologies and descriptions

--- apps/web/src/components/Home/skills.tsx
+++ apps/web/src/components/Home/skills.tsx
@@ -1,20 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
 import { getSkills } from "@/server/actions/skills";
+import { Skill as SkillType } from "@/server/types/skills";
 import React from "react";
 
 const SkillIcon = ({ icon }: { icon: string }) => (
-  console.log(icon),
-  <img src={icon} alt="Skill Icon" className="size-18" />
+  console.log(icon), (<img src={icon} alt="Skill Icon" className="size-18" />)
+);
+
+const Skill = ({ skill }: { skill: SkillType }) => (
+  <div
+    key={skill.id}
+    className="flex flex-col items-center justify-center gap-2 p-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+  >
+    <SkillIcon icon={skill.icon} />
+    <h3 className="text-xl font-semibold">{skill.name}</h3>
+    <p className="text-sm text-gray-600">{skill.description}</p>
+  </div>
 );
 
 const Skills = async () => {
-
   const skills = await getSkills();
 
   return (
-    <div className="flex gap-4">
-      <SkillIcon icon={skills[0]?.icon} />
-      <SkillIcon icon={skills[1]?.icon} />
+    <div className="flex flex-col gap-4 w-full items-center justify-center">
+      <h2 className="text-3xl font-bold tracking-wide">Minhas habilidades</h2>
+      <div className="w-full grid grid-cols-5 gap-4 items-center justify-center">
+        {skills.map((skill) => (
+          <Skill key={skill.id} skill={skill} />
+        ))}
+      </div>
     </div>
   );
 };
